d5532ac8b66a96ead1744da9101bf02afbe3ab9b
Zohar Williams <zoharkiks@gmail.com>
2023-12-30 02:52:35 +0530

    Add animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,35 @@
-// Js
+import { gsap } from "gsap";
+
+initScript();
+
+// Loader Animations
+function initLoader() {
+  var tl = gsap.timeline();
+
+  tl.fromTo(
+    ".padding_animation",
+    { padding: "0 0" },
+    { padding: "12px 12px", duration: 0.75 },
+    1
+  );
+  tl.fromTo(
+    ".bg_animation",
+    { borderRadius: "0 0 0 0", backgroundColor: "#dda15e" },
+    { borderRadius: "8px", backgroundColor: "#283618", duration: 1 },
+    1
+  );
+  tl.fromTo(
+    ".logo_animation",
+    { opacity: 0 },
+    { opacity: 1, duration: 1, delay: 0.5 }
+  );
+  tl.fromTo(
+    ".loaderQuote_animation",
+    { opacity: 0 },
+    { opacity: 1, duration: 1 }
+  );
+}
+
+function initScript() {
+  initLoader();
+}
